279d0c8616a5ce4ecbe80387dfd4a061612018fb
aslez <aslez@virginia.edu>
2016-03-01 11:27:33 -0500

    Updated documentation

--- R/1-discorr.R
+++ R/1-discorr.R
@@ -1,3 +1,42 @@
+#'Construct Distance and Correlation Matrices
+#'
+#'\code{discorr} is used to construct the distance and correlation matrices 
+#'needed to estimate range, organization, and strength.
+#'
+#'@param df a fema-class data.frame.
+#'
+#'@param longlat logical indicating whether coordinates are projected.  Default
+#'is \code{FALSE}. 
+#'
+#'@param ctype a string indicating the type of correlation to be caluclated.
+#'Valid options include single1 (cosine similarity for a single slope), 
+#'single2 (linear similarity for a single slope), 
+#'and multi (square root of the sum of squared differences across 
+#'multiple slopes). 
+#'
+#'@param x1 a vector of slopes associated with observation i.
+#'
+#'@param x2 a vector of slopes associated with observation j.
+#' 
+#'@details \code{discorr} is used to generate distance and correlation matrices
+#'that can then be summarized in terms of range, organization, and strength.  
+#'\code{pwc} calculates pairwise correlations using the methods outlined by 
+#'Martin, Slez, and Borkenhagen (2016).
+#'
+#'@return The function \code{discorr} returns the following list of objects:
+#' \describe{
+#'   \item{coords}{matrix of coordinates.}
+#'   \item{slopes}{vectors of slopes.}
+#'   \item{longlat}{logical indicating whether coordinates are projected.}
+#'   \item{dmat}{the distance matrix.}
+#'   \item{rmat}{the correlation matrix.}
+#' }
+#'
+#'@references Martin, J.L., Slez, A., and Borkenhagen, C.  2016.  
+#'"Some Provisional Techniques for Quantifying the Degree of Field Effect
+#'in Social Data."
+#'
+
 discorr <- function(df, longlat = FALSE, ctype = 'single1') {
   if (!'fema.df' %in% class(df)) {
     stop('df is not a fema-class data frame')
@@ -29,4 +68,19 @@ discorr <- function(df, longlat = FALSE, ctype = 'single1') {
                  dmat = dmat, rmat = rmat) 
   class(result) <- 'fema'
   result
+}
+
+#'@rdname discorr
+pwc <- function(x1, x2, ctype) {
+  x1x2 <- data.frame(x1 = x1, x2 = x2)
+  if (ctype == 'single1') {
+    x1x2 <- rbind(1, x1x2)
+    cpx1 <- with(x1x2, crossprod(x1))
+    cpx2 <- with(x1x2, crossprod(x2))
+    cpx1x2 <- with(x1x2, crossprod(x1, x2))
+    r <- cpx1x2 / sqrt(cpx1 * cpx2)
+  }
+  if (ctype == 'single2') r <- with(x1x2, (pi - 2 * abs(x1 - x2)) / pi)
+  if (ctype == 'multi') r <- with(x1x2, sqrt(sum((x1 - x2)^2)))
+  r
 }
--- R/6-gwr2fema.R
+++ R/6-gwr2fema.R
@@ -1,3 +1,19 @@
+#'Convert GWR Output to FEMA Data Frame
+#'
+#'\code{gwr2fema} converts output produced using \code{gwr} to a data frame
+#'that can be read by \code{discorr}.
+#'
+#'@param x a gwr-class object
+#'
+#'@param slopes a string vector indicating which slopes to retain.  The default
+#'is \code{NULL}, which retains all slopes.
+#'
+#'@param intercept logical indicating whether or not to return the intercept.
+#'The default is \code{FALSE}.
+#'
+#'@return The function \code{gwr2fema} returns a fema-class data frame.
+
+
 gwr2fema <- function(x, slopes = NULL, intercept = FALSE) {
   if (class(x) != 'gwr') {
     stop('x is not a gwr-class object')
